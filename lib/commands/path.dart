76a1e3a3c9a584412a143fb3edf4747ec041539e
bsutton <bsutton@noojee.com.au>
2019-11-16 22:15:33 +1100

    delgated a number functions to the path package.

--- lib/commands/path.dart
+++ lib/commands/path.dart
@@ -1,5 +1,6 @@
 import 'package:dshell/commands/command.dart';
-import 'package:file_utils/file_utils.dart';
+
+import 'package:path/path.dart' as p;
 
 /// Removes the directory component and extension from the [path] returning
 /// just the filename sans the extension.
@@ -17,6 +18,10 @@ String basename(String path) => Path().basename(path);
 ///   > john.jpg
 /// ```
 String filename(String path) => Path().filename(path);
+
+///
+/// Returns the parent directory of the given path.
+String parent(String path) => Path().parent(path);
 
 /// Returns the filename extension without the dot.
 ///
@@ -44,16 +49,54 @@ String absolute(String path) => Path().absolute(path);
 ///
 String canonicalize(String path) => Path().canonicalize(path);
 
+/// Delegate to Path package
+/// Joins the given path parts into a single path using the current platform's [separator]. Example:
+///
+/// p.join('path', 'to', 'foo'); // -> 'path/to/foo'
+/// If any part ends in a path separator, then a redundant separator will not be added:
+///
+/// p.join('path/', 'to', 'foo'); // -> 'path/to/foo
+/// If a part is an absolute path, then anything before that will be ignored:
+///
+/// p.join('path', '/to', 'foo'); // -> '/to/foo'
+String join(String part1,
+        [String part2,
+        String part3,
+        String part4,
+        String part5,
+        String part6,
+        String part7,
+        String part8]) =>
+    Path().join(part1, part2, part3, part4, part5, part6, part7, part8);
+
 class Path extends Command {
-  String filename(String path) => FileUtils.basename(path);
-  String canonicalize(String path) => FileUtils.fullpath(path);
+  String filename(String path) => p.basename(path);
+  String canonicalize(String path) => p.canonicalize(path);
 
   String basename(String path) {
     String extension = this.extension(path);
     String filename = this.filename(path);
-    String basename = filename.substring(0, filename.length - extension.length);
+    String basename =
+        filename.substring(0, filename.length - (extension.length + 1));
 
     return basename;
+  }
+
+  /// Returns the parent directory of path.
+  String parent(String path) {
+    String canon = p.canonicalize(path);
+    return p.dirname(canon);
+  }
+
+  String join(String part1,
+      [String part2,
+      String part3,
+      String part4,
+      String part5,
+      String part6,
+      String part7,
+      String part8]) {
+    return p.join(part1, part2, part3, part4, part5, part6, part7, part8);
   }
 
   String extension(String path) {
@@ -61,7 +104,7 @@ class Path extends Command {
 
     int index = path.lastIndexOf(".");
     if (index != -1) {
-      extension = path.substring(index);
+      extension = path.substring(index + 1);
     }
     return extension;
   }
