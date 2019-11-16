c9cd7a626a2c6b883628663e85fcb9ff35434443
bsutton <bsutton@noojee.com.au>
2019-11-16 22:14:54 +1100

    added command to fetch an environment var.

---
+++ lib/commands/env.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'command.dart';
+import 'settings.dart';
+import '../util/log.dart';
+
+/// Gets an environment variable
+///
+String env(String name) => Env().env(name);
+
+class Env extends Command {
+  static Env _self = Env._internal();
+  Map<String, String> envVars;
+
+  factory Env() {
+    return _self;
+  }
+
+  Env._internal() {
+    envVars = Platform.environment;
+  }
+
+  String env(String name) {
+    if (Settings().debug_on) {
+      Log.d("name:  ${name}");
+    }
+    return envVars[name];
+  }
+}
