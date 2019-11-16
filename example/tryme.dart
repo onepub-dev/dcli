5f3b6373cba0e8d308eb45561deb92820173e368
bsutton <bsutton@noojee.com.au>
2019-11-16 22:14:19 +1100

    moved to examples.

---
+++ example/tryme.dart
@@ -0,0 +1,30 @@
+#! /usr/bin/env dshell
+import 'package:dshell/dshell.dart';
+
+void main() {
+  echo("Hello World");
+  echo("Where are we: ${pwd}?");
+
+  makeDir("test");
+  push("test");
+  touch("icon.png");
+  touch("logo.png");
+  touch("dog.png");
+
+  // print all the file names in the current directory.
+  fileList.forEach((file) => print("Found: ${file}"));
+
+  touch("subdir/monkey.png");
+
+  // do a recursive find
+  find("*.png").forEach((file) => print("$file"));
+
+  // now cleanup
+  delete("icon.png");
+  delete("logo.png");
+  delete("dog.png");
+
+  pop();
+
+  'grep touch tryme.dart'.forEach((line) => print("Found: $line"));
+}
