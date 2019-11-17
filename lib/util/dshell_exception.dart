5080be83ea2cf26085d22181476bd24e54d6d7c3
bsutton <bsutton@noojee.com.au>
2019-11-18 08:05:25 +1100

    implemented base DshellException.

---
+++ lib/util/dshell_exception.dart
@@ -0,0 +1,24 @@
+import 'stack_trace_impl.dart';
+
+class DShellException implements Exception {
+  final String message;
+  final StackTraceImpl stackTrace;
+
+  DShellException(this.message, [StackTraceImpl stackTrace])
+      : this.stackTrace = stackTrace ?? StackTraceImpl(skipFrames: 2);
+
+  DShellException copyWith(StackTraceImpl stackTrace) {
+    return DShellException(this.message, stackTrace);
+  }
+
+  String toString() {
+    return "An Exception was thrown: ${message}";
+  }
+
+  void printStackTrace() {
+    print(stackTrace.formatStackTrace());
+  }
+
+  DShellException.from(Object exception, this.stackTrace)
+      : message = exception.toString();
+}
