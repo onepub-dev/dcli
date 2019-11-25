d01b62021342e8531422d47c2f655349bff8f83b
bsutton <bsutton@noojee.com.au>
2019-11-25 22:39:00 +1100

    added missing quote.

--- test/test_scripts/hello_world.dart
+++ test/test_scripts/hello_world.dart
@@ -1,3 +1,17 @@
+#! /usr/bin/env dshell
+/*
+@pubspec.yaml
+name: hello_world.dart
+dependencies:
+  dshell: ^1.0.0
+  money2: ^1.0.0
+*/
+
+import 'dart:io';
+import 'package:dshell/dshell.dart';
+import 'package:path/path.dart' as p;
+import 'package:money2/money2.dart';
+
 void main() {
-  print("hello world");
+  print("Yep, this is all of it.");
 }
