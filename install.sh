e8eea791e4a65c8b3650e8b596cc41ae59799b54
bsutton <bsutton@noojee.com.au>
2019-11-28 22:57:14 +1100

    Updated to install dshell in /usr/bin

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@ echo Compiling
 /usr/lib/dart/bin/dart2native bin/main.dart -o bin/dshell
 echo Installing
 
-# update this line to point to the root of your flutter install.
-FLUTTER_HOME=~/apps
-cp bin/dshell ${FLUTTER_HOME}/flutter/bin/cache/dart-sdk/bin
+cp bin/dshell  /usr/bin
