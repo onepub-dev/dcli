ccd5e2d25e04573754e73ee930c21ad1c9d7943f
bsutton <bsutton@noojee.com.au>
2019-12-10 19:36:37 +1100

    moved paths to Settings()

--- test/commands/pub_get_test.dart
+++ test/commands/pub_get_test.dart
@@ -3,10 +3,10 @@
 import 'dart:io';
 
 import 'package:dshell/script/dart_sdk.dart';
-import 'package:dshell/script/project_cache.dart';
 import 'package:dshell/script/pub_get.dart';
 import 'package:dshell/script/script.dart';
 import 'package:dshell/script/virtual_project.dart';
+import 'package:dshell/settings.dart';
 import 'package:test/test.dart';
 
 import 'package:path/path.dart' as p;
@@ -14,20 +14,18 @@ import 'package:path/path.dart' as p;
 String script = "test/test_scripts/hello_world.dart";
 
 String cwd = Directory.current.path;
-String cachePath = ProjectCache().path;
 
 String scriptDir = p.join(cwd, script);
 String scriptPath = p.dirname(scriptDir);
 String scriptName = p.basenameWithoutExtension(scriptDir);
-String projectPath =
-    p.join(cachePath, scriptPath.substring(1), scriptName + ".project");
+String projectPath = p.join(
+    Settings().cachePath, scriptPath.substring(1), scriptName + ".project");
 
 void main() {
   group("Pub Get", () {
     test('Do it', () {
-      String cachePath = ProjectCache().path;
       Script script = Script.fromFile("test/test_scripts/hello_world.dart");
-      VirtualProject project = VirtualProject(cachePath, script);
+      VirtualProject project = VirtualProject(Settings().cachePath, script);
       DartSdk sdk = DartSdk();
       PubGet(sdk, project).run();
     });
