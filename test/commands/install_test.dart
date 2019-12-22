287d15d183b85e9e00947aa20fad5e7fad2a2176
bsutton <bsutton@noojee.com.au>
2019-12-22 14:12:45 +1100

    Added extra test and tried to get it working with the MemoryFileSystem but thats missing the open method.

--- test/commands/install_test.dart
+++ test/commands/install_test.dart
@@ -1,20 +1,24 @@
-
+import 'package:dshell/dshell.dart' hide equals;
+import 'package:dshell/functions/env.dart';
 import 'package:dshell/functions/is.dart';
 import 'package:dshell/script/entry_point.dart';
 import 'package:dshell/util/dshell_exception.dart';
 import 'package:test/test.dart';
 
-
 import '../util/test_fs_zone.dart';
 import '../util/test_paths.dart';
+import '../util/wipe.dart';
 
 String script = 'test/test_scripts/hello_world.dart';
 
 void main() {
   group('Install DShell', () {
-    test('Install with success', () {
+    test('clean install', () {
       TestZone().run(() {
         try {
+          // setEnv('HOME', '/home/test');
+          // createDir('/home/test', recursive: true);
+          wipe();
           EntryPoint().process(['install']);
         } on DShellException catch (e) {
           print(e);
@@ -23,9 +27,24 @@ void main() {
         checkInstallStructure(TestPaths(script));
       });
     });
+    test('install over existing', () {
+      TestZone().run(() {
+        try {
+          // setEnv('HOME', '/home/test');
+          // createDir('/home/test', recursive: true);
+          EntryPoint().process(['install']);
+        } on DShellException catch (e) {
+          print(e);
+        }
+
+        checkInstallStructure(TestPaths(script));
+      });
+    });
+
     test('Install with error', () {
       TestZone().run(() {
         var paths = TestPaths(script);
+        wipe();
         try {
           EntryPoint().process(['install', 'a']);
         } on DShellException catch (e) {
