978377ba2c37cc13c55c4974fedbd24259dd635d
bsutton <bsutton@noojee.com.au>
2019-12-22 14:13:59 +1100

    I've commented out the file system override for the moement as the MemoryFileSystem doesn't implement 'open' on the File which causes many tests to faile.

--- test/util/test_fs_zone.dart
+++ test/util/test_fs_zone.dart
@@ -16,24 +16,26 @@ class TestZone {
   }
 
   void run<R>(R Function() body) {
-    IOOverrides.runZoned(body,
-        createDirectory: (String path) => _fs.directory(path),
-        createFile: (String path) => _fs.file(path),
-        createLink: (String path) => _fs.link(path),
-        getCurrentDirectory: () => _fs.currentDirectory,
-        setCurrentDirectory: (String path) => _fs.currentDirectory = path,
-        getSystemTempDirectory: () => _fs.systemTempDirectory,
-        stat: (String path) => _fs.stat(path),
-        statSync: (String path) => _fs.statSync(path),
-        fseIdentical: (String p1, String p2) => _fs.identical(p1, p2),
-        fseIdenticalSync: (String p1, String p2) => _fs.identicalSync(p1, p2),
-        fseGetType: (String path, bool followLinks) =>
-            _fs.type(path, followLinks: followLinks),
-        fseGetTypeSync: (String path, bool followLinks) =>
-            typeSync(path, followLinks),
-        fsWatch: (String a, int b, bool c) =>
-            throw UnsupportedError('unsupported'),
-        fsWatchIsSupported: () => _fs.isWatchSupported);
+    IOOverrides.runZoned(
+      body,
+      // createDirectory: (String path) => _fs.directory(path),
+      // createFile: (String path) => _fs.file(path),
+      // createLink: (String path) => _fs.link(path),
+      // getCurrentDirectory: () => _fs.currentDirectory,
+      // setCurrentDirectory: (String path) => _fs.currentDirectory = path,
+      // getSystemTempDirectory: () => _fs.systemTempDirectory,
+      // stat: (String path) => _fs.stat(path),
+      // statSync: (String path) => _fs.statSync(path),
+      // fseIdentical: (String p1, String p2) => _fs.identical(p1, p2),
+      // fseIdenticalSync: (String p1, String p2) => _fs.identicalSync(p1, p2),
+      // fseGetType: (String path, bool followLinks) =>
+      //     _fs.type(path, followLinks: followLinks),
+      // fseGetTypeSync: (String path, bool followLinks) =>
+      //     typeSync(path, followLinks),
+      // fsWatch: (String a, int b, bool c) =>
+      //     throw UnsupportedError('unsupported'),
+      // fsWatchIsSupported: () => _fs.isWatchSupported
+    );
   }
 
   FileSystemEntityType typeSync(String path, bool followLinks) {
@@ -41,5 +43,4 @@ class TestZone {
 
     return _fs.typeSync(path, followLinks: followLinks);
   }
-
 }
