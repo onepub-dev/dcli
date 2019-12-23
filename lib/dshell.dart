484d3109314b073a3a11b620a4dc5c425d18a90c
bsutton <bsutton@noojee.com.au>
2019-12-23 11:31:34 +1100

    reduced the public api to what is currently supported.

--- lib/dshell.dart
+++ lib/dshell.dart
@@ -13,7 +13,20 @@ export 'src/functions/head.dart' show head;
 export 'src/functions/is.dart' show isFile, isDirectory, exists;
 export 'src/functions/create_dir.dart' show createDir, CreateDirException;
 export 'src/functions/move.dart' show move, MoveException;
-export 'package:path/path.dart';
+export 'package:path/path.dart'
+    hide
+        PathMap,
+        PathSet,
+        Style,
+        Context,
+        context,
+        posix,
+        style,
+        url,
+        windows,
+        hash,
+        prettyUri,
+        toUri;
 export 'src/functions/pop.dart' show pop, PopException;
 export 'src/functions/push.dart' show push, PushException;
 export 'src/functions/pwd.dart' show pwd;
@@ -24,4 +37,4 @@ export 'src/settings.dart' show Settings;
 export 'src/functions/touch.dart' show touch, TouchException;
 export 'src/functions/which.dart' show which;
 export 'src/util/string_as_process.dart';
-export 'src/util/ansi_color.dart';
+export 'src/util/ansi_color.dart' hide AnsiColor;
