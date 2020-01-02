e35970840a49e0d8ee7a5bfb57a022aa39598a3a
bsutton <bsutton@noojee.com.au>
2020-01-02 13:14:39 +1100

    typo fix in comment.

--- test/util/wipe.dart
+++ test/util/wipe.dart
@@ -1,6 +1,6 @@
 import 'package:dshell/dshell.dart';
 
-/// Wipes the entire HOME.dshell directory tree.
+/// Wipes the entire HOME/.dshell directory tree.
 void wipe() {
   var dshellPath = join(env('HOME'), '.dshell');
   if (exists(dshellPath)) {
