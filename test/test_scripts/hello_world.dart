b92565ee4af62f4e3e1a958b86cefb91ecc8f96d
bsutton <bsutton@noojee.com.au>
2020-01-02 13:14:26 +1100

    regnerated with correct quotes and capitialisation.

--- test/test_scripts/hello_world.dart
+++ test/test_scripts/hello_world.dart
@@ -1,13 +1,6 @@
 #! /usr/bin/env dshell
-/*
-@pubspec
-name: hello_world.dart
-dependencies:
-  money2: ^1.0.0
-*/
-
 import 'package:dshell/dshell.dart';
 
 void main() {
-  print('Hello world');
+  print('Hello World');
 }
