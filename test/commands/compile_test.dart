a6db96bf6e39fa848d3b5cdd9e8d2908b7976943
bsutton <bsutton@noojee.com.au>
2020-01-02 13:13:50 +1100

    removed the call to wipe() as it was a copy/paste bug from the install test scripts.

--- test/commands/compile_test.dart
+++ test/commands/compile_test.dart
@@ -1,9 +1,10 @@
+@Timeout(Duration(seconds: 600))
+
 import 'package:dshell/src/script/entry_point.dart';
 import 'package:dshell/src/util/dshell_exception.dart';
 import 'package:test/test.dart';
 
 import '../util/test_fs_zone.dart';
-import '../util/wipe.dart';
 
 String script = 'test/test_scripts/hello_world.dart';
 
@@ -15,7 +16,6 @@ void main() {
         try {
           // setEnv('HOME', '/home/test');
           // createDir('/home/test', recursive: true);
-          wipe();
           exit = EntryPoint().process(['compile', 'example/dsort.dart']);
         } on DShellException catch (e) {
           print(e);
@@ -30,7 +30,6 @@ void main() {
         try {
           // setEnv('HOME', '/home/test');
           // createDir('/home/test', recursive: true);
-          wipe();
           exit = EntryPoint().process(['compile', '-nc', 'example/dsort.dart']);
         } on DShellException catch (e) {
           print(e);
