2a394f798d8b0b1cd037ea194d137b74e3487df1
bsutton <bsutton@noojee.com.au>
2020-01-16 09:06:28 +1100

    Disabled until dart gets fixed.

--- test/functions/mem_fs_test.dart
+++ test/functions/mem_fs_test.dart
@@ -3,11 +3,12 @@ import 'package:test/test.dart' as t;
 import '../util/test_fs_zone.dart';
 
 void main() {
+  /// waiting of dart fixed to Uri.base
   t.test('Test Zone CWD', () {
     // Test for dart bug
     // https://github.com/dart-lang/sdk/issues/39796
     TestZone().run(() {
       t.expect(Uri.base, t.equals('.'));
     });
-  });
+  }, skip: true);
 }
