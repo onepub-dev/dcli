2ae79a04f2ef18c9d3fe5a8c1613fbdcde3f165c
bsutton <bsutton@noojee.com.au>
2020-01-16 09:07:11 +1100

    disabled for now as it slows down unit testing and its not actually testing what it states it does.

--- test/test_scripts/slow_test.dart
+++ test/test_scripts/slow_test.dart
@@ -5,10 +5,21 @@ dependencies:
   dshell: ^1.0.0
   money2: ^1.0.0
 */
+import 'package:test/test.dart';
+import 'package:dshell/dshell.dart' hide equals;
 
-import 'package:dshell/dshell.dart';
+import '../util/test_fs_zone.dart';
 
 void main() {
-  'bash /home/bsutton/git/dshell/test/test_scripts/slow.sh'
-      .forEach((line) => print(line));
+  // This is intended to demonstrate that we ouput data as it flows in
+  // I'm not certain how to actually test that so for the moment this test is disabled.
+  test('Slow', () {
+    TestZone().run(() {
+      print('$pwd');
+      'bash /home/bsutton/git/dshell/test/test_scripts/slow.sh'
+          .forEach((line) => print(line));
+      expect(() => 'tail -n 5 badfilename.txt'.run,
+          throwsA(TypeMatcher<DShellException>()));
+    });
+  }, skip: true);
 }
