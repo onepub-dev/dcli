e584b1b60e1079b4dbea86fadb34c6c0dde211a2
bsutton <bsutton@noojee.com.au>
2020-01-21 08:12:52 +1100

    added hidden option to ask , renamed yesNo to confirm. toLower option now works.

--- lib/src/functions/ask.dart
+++ lib/src/functions/ask.dart
@@ -1,12 +1,17 @@
+import 'dart:cli';
 import 'dart:convert';
 import 'dart:io';
+
+import 'package:dshell/src/util/waitForEx.dart';
 
 import '../settings.dart';
 
 import '../util/log.dart';
+import '../util/string_as_process.dart';
 
 import 'dshell_function.dart';
 import 'echo.dart';
+import 'env.dart';
 
 ///
 /// Reads a line of text from stdin with an optional prompt.
@@ -25,8 +30,15 @@ import 'echo.dart';
 /// If [prompt] set then the prompt will be printed
 /// to the console and the cursor placed immediately after the prompt.
 ///
-/// if [multiline] is true then
-String ask({String prompt}) => Ask().ask(prompt: prompt);
+/// if [toLower] is true then the returned result is converted to lower case
+/// this can be useful if you need to compare the entered value.
+///
+/// If [hidden] is true then the entered values will not be echoed to the
+/// console, instead '*' will be displayed. This is uesful for capturing
+/// passwords.
+///
+String ask({String prompt, bool toLower = false, bool hidden = false}) =>
+    Ask().ask(prompt: prompt, toLower: toLower, hidden: hidden);
 
 /// [yesNo] is a specialized version of ask that returns true or
 /// false based on the value entered.
@@ -38,7 +50,7 @@ bool confirm({String prompt}) {
   bool result;
   var matched = false;
   while (!matched) {
-    var entered = Ask().ask(prompt: prompt, toLower: true);
+    var entered = Ask().ask(prompt: prompt, toLower: true, hidden: false);
     var lower = entered.toLowerCase();
 
     if (['y', 't', 'true', 'yes'].contains(lower)) {
@@ -56,25 +68,100 @@ bool confirm({String prompt}) {
   return result;
 }
 
+///
+/// Launches the systems default cli editor on Linux and MacOS
+/// using the EDITOR environment variable.
+/// If the EDITOR environment variable isn't found then
+/// we check for the VISUAL environment variable.
+/// If neither is found we use vi.
+///
+/// On Windows we launch notepad.
+///
+/// EXPERIMENTAL - does not work, do not use.
+void showEditor(String path) {
+  String editor;
+
+  if (Platform.isWindows) {
+    editor = 'notepad.exe';
+  } else {
+    editor = env('EDITOR');
+    editor ??= env('VISIBLE');
+    editor ??= 'vi';
+  }
+
+  // https://github.com/git/git/blob/master/editor.c
+  print('running $editor $path');
+  '$editor $path'.start(runInShell: true);
+
+  //(stdioType(stdin) == StdioType.terminal
+
+  // waitFor<void>(Process.start(editor, [path]).then((process) {
+  //   stdin.pipe(process.stdin);
+  // }));
+}
+
+bool isTerminalDumb() {
+  {
+    var terminal = env('TERM');
+    return terminal == null || terminal == 'dumb';
+  }
+}
+
 class Ask extends DShellFunction {
   ///
   /// Reads user input from stdin and returns it as a string.
-  String ask({String prompt, bool toLower}) {
+  String ask({String prompt, bool toLower, bool hidden}) {
     if (Settings().debug_on) {
       Log.d('ask:  ${prompt}');
     }
     if (prompt != null) {
       echo(prompt + ' ', newline: false);
     }
-    var line = stdin.readLineSync(
-        encoding: Encoding.getByName('utf-8'), retainNewlines: false);
+
+    String line;
+    if (hidden == true) {
+      line = readHidden();
+    } else {
+      line = stdin.readLineSync(
+          encoding: Encoding.getByName('utf-8'), retainNewlines: false);
+    }
 
     line ??= '';
+
+    if (toLower == true) {
+      line = line.toLowerCase();
+    }
 
     if (Settings().debug_on) {
       Log.d('ask:  result ${line}');
     }
 
     return line;
+  }
+
+  String readHidden() {
+    var line = <int>[];
+
+    try {
+      stdin.echoMode = false;
+      stdin.lineMode = false;
+      int char;
+      do {
+        char = stdin.readByteSync();
+        if (char != 10) {
+          stdout.write('*');
+          // we must wait for flush as only one flush can be outstanding at a time.
+          waitForEx<void>(stdout.flush());
+          line.add(char);
+        }
+      } while (char != 10);
+    } finally {
+      stdin.echoMode = true;
+      stdin.lineMode = true;
+    }
+
+    // output a newline as we have suppressed it.
+    print('');
+    return Encoding.getByName('utf-8').decode(line);
   }
 }
---
+++ test/test_scripts/read_hidden.dart
@@ -0,0 +1,39 @@
+#! /usr/bin/env dshell
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dshell/dshell.dart';
+import 'package:dshell/src/util/waitForEx.dart';
+
+void main() {
+  print('confirmd ${confirm(prompt: "Do you agree?")}');
+
+  print("user: ${ask(prompt: 'username:', toLower: true)}");
+
+  print("password: ${ask(prompt: 'password:', hidden: true)}");
+}
+
+String readHidden() {
+  var line = <int>[];
+
+  print('read hidden');
+
+  try {
+    stdin.echoMode = false;
+    stdin.lineMode = false;
+    int char;
+    do {
+      char = stdin.readByteSync();
+      if (char != 10) {
+        stdout.write('*');
+        waitForEx<void>(stdout.flush());
+        line.add(char);
+      }
+    } while (char != 10);
+  } finally {
+    stdin.echoMode = true;
+    stdin.lineMode = true;
+  }
+
+  return Encoding.getByName('utf-8').decode(line);
+}
