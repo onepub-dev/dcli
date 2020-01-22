4ec7425a498b62b62277d383c4eeef3a8a42332e
bsutton <bsutton@noojee.com.au>
2020-01-22 16:30:22 +1100

    experimentations with pipes.

---
+++ test/test_scripts/pipe_for_stack.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  var ls = await start('ls', []);
+  var head = await start('head', ['-n', '5']);
+
+   var cnt = 1;
+  ls.stdout
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .map((line) {
+        print(line);
+        return '${cnt++}: $line';
+      })
+      .transform(utf8.encoder)
+      .pipe(head.stdin);
+
+  await head.stdout.transform(streamTransformer).pipe(stdout);
+}
+
+Future<Process> start(String command, List<String> args) async {
+  var process = Process.start(
+    command,
+    args,
+  );
+  return process;
+}
---
+++ test/test_scripts/pipe_for_stack3.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  //var ls = await start('ls');
+  var head = await start('head');
+  var tail = await start('tail');
+
+  run(generateLines(100), head, tail);
+}
+
+void run(Stream<String> ls, Process head, Process tail) {
+  var cnt = 0;
+  var fls = ls
+      .transform(const LineSplitter())
+      .map((line) => '${++cnt}: $line\n')
+      .transform(utf8.encoder)
+      .pipe(head.stdin)
+      .catchError((Object e, StackTrace s) async {
+    print('head exit: ${await head.exitCode}');
+  },
+          test: (e) =>
+              e is SocketException && e.osError.errorCode == 32 // broken  pipe'
+          );
+
+  var fhead = head.stdout
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .map((line) => 'tail: $line\n')
+      .transform(utf8.encoder)
+      .pipe(tail.stdin)
+      .catchError((Object e, StackTrace s) async {
+    print('tail exit: ${await tail.exitCode}');
+  }, test: (e) => e is SocketException && e.osError.message == 'Broken pipe');
+
+  var ftail = tail.stdout.pipe(stdout);
+
+  Future.wait<void>([fls, fhead, ftail]);
+}
+
+Future<Process> start(String command) async {
+  var process = Process.start(
+    command,
+    [],
+  );
+  return process;
+}
+
+Stream<String> generateLines(int to) async* {
+  for (var i = 0; i < to; i++) {
+    print('gen $i');
+    yield 'generated($i)\n';
+  }
+}
