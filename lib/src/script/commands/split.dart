f831a18e6afd3dabfc5bec10f9671aa836f0afcb
bsutton <bsutton@noojee.com.au>
2020-01-22 16:30:00 +1100

    improvements to split to handle additional scenarios.

--- lib/src/script/commands/split.dart
+++ lib/src/script/commands/split.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:dshell/dshell.dart';
+import 'package:dshell/src/functions/is.dart';
 
 import '../command_line_runner.dart';
 import '../flags.dart';
@@ -12,13 +14,30 @@ class SplitCommand extends Command {
 
   @override
   int run(List<Flag> selectedFlags, List<String> subarguments) {
+    var exitCode = 0;
     if (subarguments.isEmpty) {
       throw InvalidArguments('Split requires a argument <script file.dart>');
     }
     Script.validate(subarguments);
-    Script.fromFile(subarguments[0]);
+    var script = Script.fromFile(subarguments[0]);
 
-    return 0;
+    if (exists(join(script.path, 'pubspec.yaml'))) {
+      if (_identical(script)) {
+        print('The pubspec.yaml already exists and is upto date');
+      } else {
+        printerr(
+            'A pubspec.yaml already exists in ${script.path}, however it is not up to date');
+        printerr('Delete the existing pubspec.yaml and try again.');
+        exitCode = 1;
+      }
+    } else {
+      copy(script.pubSpecPath, join(script.path, 'pubspec.yaml'));
+      // now we need to disable the @pubspec annotation.
+      _replace(script.path, '@pubspec', '@disabled-pubspec');
+      print('complete.');
+    }
+
+    return exitCode;
   }
 
   @override
@@ -38,5 +57,42 @@ class SplitCommand extends Command {
       }
     }
     return results;
+  }
+
+  // checks if the script's pubspec is identical to the
+  // pubspec in the local directory
+  bool _identical(Script script) {
+    var localPubspecPath = canonicalize(join(script.path, 'pubspec.yaml'));
+
+    // check the virtual project has a symlink back to the local pubspec.
+    if (isLink(script.pubSpecPath)) {
+      var resolved = resolveSymLink(script.pubSpecPath);
+      if (resolved == localPubspecPath) {
+        return true;
+      }
+    }
+
+    return _compare(localPubspecPath, script.pubSpecPath);
+  }
+
+  void _replace(String path, String existing, String replacement) {
+    var tmp = '$path.tmp';
+    if (exists(tmp)) {
+      delete(tmp);
+    }
+    read(path).forEach((line) {
+      line = line.replaceFirst(existing, replacement);
+      tmp.append(line);
+    });
+    move(path, '$path.bak');
+    move(tmp, path);
+    delete('$path.bak');
+  }
+
+  bool _compare(String localPubspecPath, String pubSpecPath) {
+    var localContent = read(localPubspecPath).toList();
+    var virtualContent = read(pubSpecPath).toList();
+
+    return const ListEquality<String>().equals(localContent, virtualContent);
   }
 }
