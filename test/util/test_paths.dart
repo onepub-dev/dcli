649c29e40a09669e0e78c66f804d8a585c68f0bd
bsutton <bsutton@noojee.com.au>
2020-01-25 13:07:14 +1100

    Added logic to do an install of dshell when the test suite starts.

--- test/util/test_paths.dart
+++ test/util/test_paths.dart
@@ -6,6 +6,39 @@ import 'package:dshell/src/script/virtual_project.dart';
 import 'package:dshell/src/util/pub_cache.dart';
 import 'package:path/path.dart';
 
+/// TestPaths sets up an isolated area for unit tests to run without
+/// interfering with your normal dshell install.
+///
+/// To do this it modifies the folling environment variables:
+///
+/// HOME = /tmp/dshell/home
+/// PUB_CACHE = /tmp/dshell/.pub_cache
+///
+/// The dshell cache is therefore located at:
+///
+/// /tmp/dshell/cache
+///
+/// As the unit test suite creates an isolated .pub-cache it will be empty.
+/// As such when the unit tests start dshell is not actually installed in the
+/// active .pub-cache.
+///
+///
+/// The result is that dshell is neither available in .pub-cache nor installed into
+/// .dshell.
+///
+/// This is not a problem for running most unit tests as the are using the
+/// primary .pub-cache. It is however a problem if you attempt to spawn
+/// a dshell instance on the cli.
+///
+/// The first time TestPaths is called it will create the necessary paths
+/// and install dshell.
+///
+/// To ensure that the install happens at the start of each test run (and then only once)
+/// we store the test runs PID into /tmp/dshell/PID.
+/// If the PID changes we know we need to recreate and reinstall everything.
+///
+///
+
 class TestPaths {
   static final TestPaths _self = TestPaths._internal();
 
@@ -28,6 +61,20 @@ class TestPaths {
     var home = truepath(TEST_ROOT, 'home');
     setEnv('HOME', home);
 
+    var dshellPath = Settings().dshellPath;
+    if (!dshellPath.startsWith('/tmp') ||
+        !HOME.startsWith('/tmp') ||
+        !env('PUB_CACHE').startsWith('/tmp')) {
+      printerr(
+          '''Something went wrong, the dshell path or HOME for unit tests is NOT pointing to /tmp. 
+          dshell's path is pointing at $dshellPath
+          HOME is pointing at $HOME
+          PUB_CACHE is pointint ag ${env('PUB_CACHE')}
+          ''');
+      printerr('We have shutdown the unit tests to protect your filesystem.');
+      exit(1);
+    }
+
     // create test home dir
     recreateDir(home);
 
@@ -43,6 +90,8 @@ class TestPaths {
     recreateDir(pubCachePath);
 
     testScriptPath = truepath(TEST_ROOT, 'scripts');
+
+    installDshell();
   }
 
   String projectPath(String scriptName) {
@@ -66,5 +115,11 @@ class TestPaths {
       deleteDir(path, recursive: true);
     }
     createDir(path, recursive: true);
+  }
+
+  void installDshell(String pidPath) {
+    'pub global activate --source path .'.run;
+
+    'dshell install'.run;
   }
 }
