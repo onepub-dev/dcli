1a58fcddd9b614b00c2c5530d145226c7f8cf8b5
bsutton <bsutton@noojee.com.au>
2020-01-25 15:50:50 +1100

    path reorg.

--- test/functions/env_test.dart
+++ test/functions/env_test.dart
@@ -2,10 +2,11 @@ import 'package:test/test.dart' as t;
 import 'package:dshell/dshell.dart';
 
 import '../util/test_fs_zone.dart';
+import '../util/test_paths.dart';
 
 void main() {
   TestPaths();
-  
+
   Settings().debug_on = true;
 
   t.group('Environment', () {
