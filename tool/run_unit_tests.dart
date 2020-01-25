12f3a3cc9da5669eaeb592dde8fe8c1f157aa09d
bsutton <bsutton@noojee.com.au>
2020-01-26 10:57:15 +1100

    wrote a tool to run unit tests serially as vscode runs two isolites.

---
+++ tool/run_unit_tests.dart
@@ -0,0 +1,20 @@
+#! /usr/bin/env dshell
+
+import 'dart:io';
+import 'package:dshell/dshell.dart';
+
+///
+/// running unit tests from vs-code doesn't seem to work as it spawns
+/// two isolates and runs tests in parallel (even when using the -j1 option)
+/// Given we are actively modifying the file system this is a bad idea.
+/// So this script forces the test to run serially via the -j1 option.
+///
+void main() {
+
+	if (!exists('pubspec.yaml'))
+	{
+		printerr(red('This script must be run from the packages root directory.'));
+		exit(1);
+	}
+	'pub run test -j1'.run;
+}
