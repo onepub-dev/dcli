5f1f2ae55e6f25eb858e26e4362cc7a857c07c68
bsutton <bsutton@noojee.com.au>
2020-02-02 10:57:29 +1100

    [FIX] updated name of docker file to match actual file for install.clone.dart.

--- docker/install.clone.dart
+++ docker/install.clone.dart
@@ -6,5 +6,5 @@ import 'package:dshell/dshell.dart';
 void main() {
   /// --no-cache is used as we want the git clone to occur every time
   /// so we are always running of the latest version
-  'sudo docker build --no-cache -f ./Dockerfile -t dshell:install_test ..'.run;
+  'sudo docker build --no-cache -f ./install.clone.df -t dshell:install_test ..'.run;
 }
