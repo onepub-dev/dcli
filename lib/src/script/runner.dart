30e3fe24af390be43749e66d5cd1bd23e672df28
bsutton <bsutton@noojee.com.au>
2020-02-03 19:09:36 +1100

    [FIX] fixed a bug where dshell wasn't passing tty down to a spawned process.

--- lib/src/script/runner.dart
+++ lib/src/script/runner.dart
@@ -30,26 +30,10 @@ class ScriptRunner {
         'Executing: ${Platform.executable} $vmArgs, in: ${project.script.scriptDirectory}');
 
     // Execute the script
-    final process =
-        waitFor<Process>(Process.start(Platform.executable, vmArgs));
-
-    // Pipe std out and in
-    final StreamSubscription stderrSub =
-        process.stderr.listen((List<int> d) => stderr.add(d));
-    final StreamSubscription stdoutSub =
-        process.stdout.listen((List<int> d) => stdout.add(d));
-    final StreamSubscription stdinSub =
-        stdin.listen((List<int> d) => process.stdin.add(d));
+    final process = waitFor<Process>(Process.start(Platform.executable, vmArgs,
+        mode: ProcessStartMode.inheritStdio));
 
     final exitCode = waitForEx<int>(process.exitCode);
-
-    final futures = <Future<void>>[];
-
-    futures.add(stderrSub.cancel());
-    futures.add(stdoutSub.cancel());
-    futures.add(stdinSub.cancel());
-
-    waitFor<void>(Future.wait(futures));
 
     return exitCode;
   }
