a0a965f3523232eb42b7d4bc3c67e7cca1b1ad38
bsutton <bsutton@noojee.com.au>
2020-02-04 23:49:56 +1100

    [ENH] Added support for pid detection on windows.

--- lib/src/util/process_helper.dart
+++ lib/src/util/process_helper.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:csv/csv.dart';
 import 'package:dshell/dshell.dart';
 
 ///
@@ -14,11 +15,19 @@ class ProcessHelper {
 
   ProcessHelper._internal();
 
+  String getPIDName(int pid) {
+    if (Platform.isWindows) {
+      return _getWindowsPidName(pid);
+    } else {
+      return _getLinuxPIDName(pid);
+    }
+  }
+
   /// Gest the process name for the given pid
   ///
   /// Throws an RunException exception if the name can't
   /// be obtained.
-  String getPIDName(int pid) {
+  String _getLinuxPIDName(int pid) {
     String line;
 
     try {
@@ -52,4 +61,79 @@ class ProcessHelper {
 
     return parentPid;
   }
+
+  bool isRunning(int lpid) {
+    if (Platform.isWindows) {
+      return _WindowsIsrunning(lpid);
+    } else {
+      return _LinuxisRunning(lpid);
+    }
+  }
+
+  bool _WindowsIsrunning(int lpid) {
+    for (var details in _getWindowsProcesses()) {
+      if (details.pid == lpid) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool _LinuxisRunning(int lpid) {
+    var isRunning = false;
+
+    String line;
+
+    try {
+      line = 'ps -q $lpid -o comm='.firstLine;
+      if (line != null) {
+        isRunning = true;
+      }
+    } on ProcessException {
+      // ps not supported on current OS
+      // we have to assume the process running
+    }
+
+    return isRunning;
+  }
+
+  /// completely untested as I don't have a windows box.
+  String _getWindowsPidName(int lpid) {
+    String pidName;
+    for (var details in _getWindowsProcesses()) {
+      if (lpid == details.pid) {
+        pidName = details.processName;
+        break;
+      }
+    }
+    return pidName;
+  }
+
+  List<PIDDetails> _getWindowsProcesses() {
+    var pids = List<PIDDetails>();
+
+    // "wininit.exe","584","Services","0","5,248 K"
+    var tasks = 'tasklist /fo csv /nh'.toList();
+
+    var lines = const CsvToListConverter().convert(tasks.join('\r\n'));
+    for (var line in lines) {
+      var details = PIDDetails();
+
+      details.processName = line[0] as String;
+      details.pid = line[1] as int;
+      details.memory = line[4] as String;
+      details.memoryUnits = line[5] as String;
+
+      pids.add(details);
+    }
+
+    return pids;
+  }
+}
+
+class PIDDetails {
+  int pid;
+  String processName;
+  String memory;
+  String memoryUnits;
 }
