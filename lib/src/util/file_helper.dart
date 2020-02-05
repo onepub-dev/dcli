670a1e321ef5b050d424d4e2d1ab18a8710ac316
bsutton <bsutton@noojee.com.au>
2020-02-06 00:27:46 +1100

    removed old version of createDir.

--- lib/src/util/file_helper.dart
+++ lib/src/util/file_helper.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import '../../dshell.dart';
 
 void writeToFile(String path, String content) {
   File _file;
@@ -12,22 +11,4 @@ void writeToFile(String path, String content) {
   _raf.writeStringSync(content);
 
   _raf.closeSync();
-}
-
-/// Returns true if the directory already
-/// existed.
-bool createDir(String dir, String description) {
-  var created = false;
-  var directory = Directory(dir);
-  if (!directory.existsSync()) {
-    try {
-      directory.createSync(recursive: true);
-      created = true;
-    } catch (e) {
-      printerr(
-          'Unable to create the $description ${dir}. Error: ${e.toString()}');
-      rethrow;
-    }
-  }
-  return !created;
 }
