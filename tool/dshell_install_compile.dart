e9d6fe1c4d0fb3fae5f060674b3e0545902045d6
bsutton <bsutton@noojee.com.au>
2020-02-06 18:11:22 +1100

    formatting.

--- tool/dshell_install_compile.dart
+++ tool/dshell_install_compile.dart
@@ -4,11 +4,11 @@ import 'package:dshell/src/script/script.dart';
 
 ///
 /// compiles a copy of dshell_install and copies it to ~/pub-cache/bin
-// Used during testing of node_mgmt/install_dshell.dart so we are certain
+// Used during testing of install_dshell.dart so we are certain
 // it has the latest version.
 void main() {
   try {
-    DartSdk()
-        .runDart2Native(Script.fromFile('../bin/dshell_install.dart'), '${HOME}/.pub-cache/bin', '.');
+    DartSdk().runDart2Native(Script.fromFile('../bin/dshell_install.dart'),
+        '${HOME}/.pub-cache/bin', '.');
   } finally {}
 }
