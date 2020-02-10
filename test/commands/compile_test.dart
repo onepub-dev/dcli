1773c85e6d4ad84cb8e1aead6b9a133362101d0b
bsutton <bsutton@noojee.com.au>
2020-02-10 13:42:40 +1100

    Added test for compiling a script in a parent directory.

--- test/commands/compile_test.dart
+++ test/commands/compile_test.dart
@@ -1,6 +1,7 @@
 // @Timeout(Duration(seconds: 600))
 
 import 'package:dshell/src/script/entry_point.dart';
+import 'package:dshell/src/settings.dart';
 import 'package:dshell/src/util/dshell_exception.dart';
 import 'package:test/test.dart';
 
@@ -34,6 +35,21 @@ void main() {
           // setEnv('HOME', '/home/test');
           // createDir('/home/test', recursive: true);
           exit = EntryPoint().process(['compile', '-nc', 'example/dsort.dart']);
+        } on DShellException catch (e) {
+          print(e);
+        }
+        expect(exit, equals(0));
+      });
+    });
+
+    test('compile a shell in a parent directory.', () {
+      TestZone().run(() {
+        Settings().reset();
+        var exit = -1;
+        try {
+          // setEnv('HOME', '/home/test');
+          // createDir('/home/test', recursive: true);
+          exit = EntryPoint().process(['-v', 'compile', '../util.dart']);
         } on DShellException catch (e) {
           print(e);
         }
