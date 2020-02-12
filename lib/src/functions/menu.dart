2fdd043a5161d16bf4f5ebb5b029dd99559ccf44
bsutton <bsutton@noojee.com.au>
2020-02-12 11:02:57 +1100

    Added an opton to control which end of a limited list the menu displays from.

--- lib/src/functions/menu.dart
+++ lib/src/functions/menu.dart
@@ -35,12 +35,21 @@ import '../../dshell.dart';
 /// If [description] is null then [option.toString()] will be used
 /// as the description for the menu option.
 ///
+/// When a [limit] is applied by default the menu will display the first [limit]
+/// options. If you specify [fromStart]: false then the menu will display the
+/// last [limit] options.
+///
 
 T menu<T>(String prompt, List<T> options,
-    {int limit, String Function(T) description}) {
+    {int limit, String Function(T) description, bool fromStart = true}) {
   limit ??= options.length;
+
+  var displayList = options;
+  if (fromStart == false) {
+    displayList = options.sublist(options.length - limit);
+  }
   for (var i = 1; i <= limit; i++) {
-    var option = options[i - 1];
+    var option = displayList[i - 1];
     String desc;
     if (description != null) {
       desc = description(option);
