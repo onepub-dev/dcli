27033f5990b5c97fded5335569d3244131f07669
Brett Sutton <bsutton@noojee.com.au>
2020-03-10 05:37:43 -0700

    grammar.

--- lib/src/script/commands/install.dart
+++ lib/src/script/commands/install.dart
@@ -133,7 +133,7 @@ class InstallCommand extends Command {
 
       if (!shell.addToPath(binPath)) {
         printerr(
-            'If you want to use dshell compile -i to install scripts;  add $binPath to your PATH.');
+            'If you want to use dshell compile -i to install scripts, add $binPath to your PATH.');
       }
     }
 
