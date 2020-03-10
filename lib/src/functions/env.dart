759732416af32a150bae58fdda07065ee8c05408
Brett Sutton <bsutton@noojee.com.au>
2020-03-10 02:35:49 -0700

    Synchronized APPDATA and HOME on Windows systems. I'm not certain this is the best idea.

--- lib/src/functions/env.dart
+++ lib/src/functions/env.dart
@@ -153,8 +153,32 @@ class Env extends DShellFunction {
   /// Passing a null [value] will remove the key from the
   /// set on environment variables.
   void setEnv(String name, String value) {
-    if (value == null) envVars.remove(name);
+    if (value == null) 
+    {
+      
+      envVars.remove(name);
+      if (Platform.isWindows){
+      if (name == 'HOME' || name =='APPDATA')
+      {
+        envVars.remove('HOME');
+        envVars.remove('APPDATA');
+      }
+      }
+    }
+    else
+    {
     envVars[name] = value;
+
+  if (Platform.isWindows)
+  {
+     if (name == 'HOME' || name =='APPDATA')
+     {
+       envVars['HOME']  = value;
+       envVars['APPDATA']  = value;
+     }
+  }
+
+    }
   }
 
   static void setMock(Env mockEnv) {
