d510fc17be22090fbd4e56fc7ac5e13e2ea936ee
bsutton <bsutton@noojee.com.au>
2020-04-01 23:19:24 +1100

    spelling.

--- bin/dshell_install.dart
+++ bin/dshell_install.dart
@@ -1,3 +1,4 @@
+#! /usr/bin/env dshell
 import 'package:dshell/src/script/entry_point.dart';
 
 void main(List<String> arguments) {
@@ -12,7 +13,7 @@ class DShellInstall {
     if (!mutableArgs.contains('doctor')) {
       // We add the 'install' so we do the install
       // by default.
-      // Unless the passed doctor.
+      // Unless they passed doctor.
       // we do an add so they can still pass global
       // switches such as -v
       mutableArgs.add('install');
