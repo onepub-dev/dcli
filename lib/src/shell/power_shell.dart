3faf39e270a6fc56b444910ecd446f5b0f1bb6d2
Brett Sutton <bsutton@noojee.com.au>
2020-07-11 17:05:34 +1000

    Moved common shell code into the mixin.

--- lib/src/shell/power_shell.dart
+++ lib/src/shell/power_shell.dart
@@ -1,10 +1,9 @@
 import '../../dshell.dart';
-import '../installers/windows_installer.dart';
-import '../script/commands/install.dart';
 import 'shell_mixin.dart';
+import 'windows_mixin.dart';
 
 /// Windows Power Shell
-class PowerShell with ShellMixin {
+class PowerShell with WindowsMixin, ShellMixin {
   /// Name of the shell
   static const String shellName = 'powershell.exe';
 
@@ -49,59 +48,4 @@ class PowerShell with ShellMixin {
 
   @override
   bool get hasStartScript => false;
-
-  @override
-  bool get isPrivilegedUser {
-    var currentPrincipal =
-        'New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())'
-            .firstLine;
-    Settings().verbose('currentPrinciple: $currentPrincipal');
-    var isPrivileged =
-        '$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)'
-            .firstLine;
-    Settings().verbose('isPrivileged: $isPrivileged');
-
-    return isPrivileged.toLowerCase() == 'true';
-  }
-
-  @override
-  String get loggedInUser => env('USERNAME');
-
-  @override
-  String privilegesRequiredMessage(String app) {
-    return 'You need to be a privileged user to run $app';
-  }
-
-  @override
-  bool install() {
-    return WindowsDShellInstaller().install();
-  }
-
-  @override
-  String checkInstallPreconditions() {
-    if (!inDeveloperMode()) {
-      return '''You must be running in Windows Developer Mode to install DShell.
-Read additional details here: https://github.com/bsutton/dshell/wiki/Installing-DShell#windows''';
-    }
-    return null;
-  }
-
-  /// Windows 10+ has a developer mode that needs to be enabled to create symlinks without escalated prividedges.
-  bool inDeveloperMode() {
-    /// Example result:
-    /// <blank line>
-    /// HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock
-    /// AllowDevelopmentWithoutDevLicense    REG_DWORD    0x1
-
-    var response =
-        'reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowDevelopmentWithoutDevLicense"'
-            .toList(skipLines: 2)
-            .first;
-    var parts = response.split(r'\s+');
-    if (parts.length != 3) {
-      throw InstallException('Unable to obtain development mode settings');
-    }
-
-    return parts[3] == '0x1';
-  }
 }
