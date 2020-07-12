51d92068ea1776be39f258e7b0976a4f8835f33f
Brett Sutton <bsutton@noojee.com.au>
2020-07-12 21:05:30 +1000

    test script for path manipulation.

---
+++ test/pubspec/setpath_test.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:dshell/dshell.dart';
+import 'package:dshell/src/util/pub_cache.dart';
+import 'package:path/path.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('setx', () {
+    var dartToolDir = r"C:\tools\dart-sdk";
+
+    /// add the dartsdk path to the windows path.
+    Env().pathPutIfAbsent(join(dartToolDir, 'bin'));
+    Env().pathPutIfAbsent(PubCache().binPath);
+    Env().pathPutIfAbsent(Settings().dshellBinPath);
+
+    print(PATH);
+
+    'setx PATH "%PATH%;${PATH.join(Env().pathDelimiter)}"'.run;
+  }, skip: !Platform.isWindows);
+
+  test('PutIfAbsent', () {
+    var dartToolDir = r"C:\tools\dart-sdk";
+
+    /// add the dartsdk path to the windows path.
+    Env().pathPutIfAbsent(join(dartToolDir, 'bin'));
+    Env().pathPutIfAbsent(PubCache().binPath);
+    Env().pathPutIfAbsent(Settings().dshellBinPath);
+
+    print(PATH);
+  });
+}
