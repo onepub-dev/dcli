a5147eb3529a04315d828c6a41aacf86e3a35334
Brett Sutton <bsutton@noojee.com.au>
2020-07-19 22:16:45 +1000

    Added tests for the more liberal parsing of annotations.

--- test/src/pubspec/pubspec_annotation_test.dart
+++ test/src/pubspec/pubspec_annotation_test.dart
@@ -2,7 +2,6 @@
 import 'package:dshell/src/pubspec/pubspec_annotation.dart';
 import 'package:test/test.dart';
 
-
 void main() {
   test('parse /*', () {
     var annotation = '''
@@ -86,6 +85,42 @@ void main() {
      *    path: ^1.6.4
     */
     ''';
+
+    var pubspec = PubSpecAnnotation.fromString(annotation);
+    expect(pubspec.annotationFound(), equals(true));
+  });
+
+  test('parse /**@pubsec', () {
+    /**
+       * 
+       */
+    var annotation = '''
+    /**@pubspec
+     *  name: find.dart
+     *  dependencies:
+     *    dshell: ^1.0.0
+     *    args: ^1.5.2
+     *    path: ^1.6.4
+    */
+    ''';
+
+    var pubspec = PubSpecAnnotation.fromString(annotation);
+    expect(pubspec.annotationFound(), equals(true));
+  });
+
+  test('parse /* @pubsec', () {
+    var annotation = '''
+    /* @pubspec
+name: test
+version: 1.0.0
+dependencies:
+  dshell: ^2.0.0
+  args: ^2.0.1
+  collection: ^1.14.12
+  file_utils: ^0.1.3
+  path: ^2.0.2
+*/
+''';
 
     var pubspec = PubSpecAnnotation.fromString(annotation);
     expect(pubspec.annotationFound(), equals(true));
