c6ca8e2b8104e7a037a9a2d92efd292ff77eb3f7
Brett Sutton <bsutton@noojee.com.au>
2020-09-18 23:52:57 +1000

    added test for warmup.

---
+++ test/src/script/commands/warmup_test.dart
@@ -0,0 +1,25 @@
+@Timeout(Duration(seconds: 600))
+
+import 'package:dcli/dcli.dart' hide equals;
+import 'package:test/test.dart';
+
+import '../../util/test_file_system.dart';
+
+String script = 'test/test_scripts/general/bin/hello_world.dart';
+
+void main() {
+  group('warmup using DCli', () {
+    test('warmup ', () {
+      TestFileSystem().withinZone((fs) {
+        var project = DartProject.fromPath('example');
+
+        project.clean();
+        project.warmup();
+
+        expect(exists(join('example', '.dart_tool')), equals(true));
+        expect(exists(join('example', '.packages')), equals(true));
+        expect(exists(join('example', '.pubspec.lock')), equals(true));
+      });
+    });
+  });
+}
