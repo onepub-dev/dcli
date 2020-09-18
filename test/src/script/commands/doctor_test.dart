ea05014c275bd03d9d7de9fe8aa33099d1af3c5e
Brett Sutton <bsutton@noojee.com.au>
2020-09-18 23:51:35 +1000

    removed unused code.

--- test/src/script/commands/doctor_test.dart
+++ test/src/script/commands/doctor_test.dart
@@ -6,8 +6,6 @@ import 'package:test/test.dart';
 
 import '../../util/test_file_system.dart';
 
-String script = 'test/test_scripts/general/bin/hello_world.dart';
-
 void main() {
   test('dcli doctor', () {
     TestFileSystem().withinZone((fs) {
