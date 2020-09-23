06cfc0516ca1e997d8ba278fe4f558d93dc157ba
Brett Sutton <bsutton@noojee.com.au>
2020-09-23 11:04:36 +1000

    reverted logic that had the script being run from the script directory as it doesn't allow people to select the directory they want to run the script in.

--- lib/src/script/runner.dart
+++ lib/src/script/runner.dart
@@ -13,8 +13,8 @@ class ScriptRunner {
   ///
   ScriptRunner(this._sdk, this.script, this._scriptArguments);
 
-  /// Executes the script
-  int exec() {
+  /// Run the script
+  int run() {
     // Prepare VM arguments
     final vmArgs = <String>[];
     vmArgs.add('--enable-asserts');
@@ -26,11 +26,12 @@ class ScriptRunner {
     vmArgs.addAll(_scriptArguments);
 
     Settings().verbose(
-        'Executing: ${DartSdk().pathToDartExe} $vmArgs, in: ${script.pathToScriptDirectory}');
+        'Executing: ${DartSdk().pathToDartExe} $vmArgs, ${script.pathToScript}');
 
     // Execute the script
     final process = waitFor<Process>(Process.start(_sdk.pathToDartExe, vmArgs,
-        mode: ProcessStartMode.inheritStdio));
+        mode: ProcessStartMode.inheritStdio,
+        workingDirectory: dirname(script.pathToScript)));
 
     final exitCode = waitForEx<int>(process.exitCode);
 
