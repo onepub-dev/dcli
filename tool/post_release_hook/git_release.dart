16b8a2706976a291a817b6a9bee649767bd614d7
Brett Sutton <bsutton@noojee.com.au>
2020-09-30 12:00:33 +1000

    Added logic to find the settings file.

--- tool/post_release_hook/git_release.dart
+++ tool/post_release_hook/git_release.dart
@@ -4,7 +4,11 @@ import 'package:dcli/dcli.dart';
 import 'package:settings_yaml/settings_yaml.dart';
 
 void main(List<String> args) {
-  var settings = SettingsYaml.load(pathToSettings: 'settings.config');
+  var project = DartProject.current;
+
+  var pathToSettings = join(project.pathToProjectRoot, 'tool',
+      'post_release_hook', 'settings.config');
+  var settings = SettingsYaml.load(pathToSettings: pathToSettings);
   var username = settings['username'] as String;
   var apiToken = settings['apiToken'] as String;
   var owner = settings['owner'] as String;
