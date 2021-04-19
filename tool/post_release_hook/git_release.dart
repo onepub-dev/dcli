6ea8b03e2dd71f30da21d4e98314dc979bff8440
Brett Sutton <bsutton@noojee.com.au>
2021-04-19 11:06:30 +1000

    renamed settings to settings.yaml. Change the name of the token field to match that used by github.

--- tool/post_release_hook/git_release.dart
+++ tool/post_release_hook/git_release.dart
@@ -7,15 +7,15 @@ import 'package:settings_yaml/settings_yaml.dart';
 void main(List<String> args) {
   final project = DartProject.current;
 
-  final pathToSettings = join(project.pathToProjectRoot, 'tool',
-      'post_release_hook', 'settings.config');
+  final pathToSettings = join(
+      project.pathToProjectRoot, 'tool', 'post_release_hook', '.settings.yaml');
   final settings = SettingsYaml.load(pathToSettings: pathToSettings);
   final username = settings['username'] as String?;
-  final apiToken = settings['apiToken'] as String?;
+  final personalAccessToken = settings['personalAccessToken'] as String?;
   final owner = settings['owner'] as String?;
   final repository = settings['repository'] as String?;
 
-  'github_release -u $username --apiToken $apiToken --owner $owner '
+  'github_release -u $username --apiToken $personalAccessToken --owner $owner '
           '--repository $repository'
       .start(workingDirectory: Script.current.pathToProjectRoot);
 }
