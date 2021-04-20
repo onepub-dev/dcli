35e320a0a3ca16eec84f2c16514fd6ed5dad1bb2
Brett Sutton <bsutton@noojee.com.au>
2021-04-20 11:26:09 +1000

    Added tests for ProcessHelp pid detection.

--- test/src/util/process_helper_test.dart
+++ test/src/util/process_helper_test.dart
@@ -1,12 +1,18 @@
 @Timeout(Duration(minutes: 5))
 import 'dart:io';
 
-import 'package:dcli/dcli.dart';
+import 'package:dcli/dcli.dart' hide equals;
 import 'package:test/test.dart';
 
 void main() {
   test('ProcessHelper', () {
-    ProcessHelper().getProcessName(pid);
+    expect(ProcessHelper().getProcessName(pid), isNot(equals('unknown')));
+  });
+
+  test('ProcessHelper', () {
+    final parent = ProcessHelper().getParentPID(pid);
+    expect(parent, isNot(equals(-1)));
+    expect(parent, isNot(equals(pid)));
   });
 
   // test('ProcessHelper', () {
