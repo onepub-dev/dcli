74902f7a988ba040548983efb10881b7dbd4298d
Brett Sutton <bsutton@noojee.com.au>
2021-05-27 17:50:21 +1000

    implemented call to SendMessageTimeout to notify top level windows when we change the environment.

---
+++ lib/src/platform/windows/message.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:win32/win32.dart';
+
+// ignore: public_member_api_docs,constant_identifier_names
+const int HWND_BROADCAST = 0xffff;
+
+// ignore: public_member_api_docs,constant_identifier_names
+const int SMTO_ABORTIFHUNG = 0x0002;
+
+/// Send a message to all top level windows that an environment variable
+/// has changed.
+void broadcastEnvironmentChange() {
+  final what = TEXT('Environment');
+
+  final pResult = calloc<Int32>(1);
+  try {
+    SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
+        what.address, SMTO_ABORTIFHUNG, 5000, pResult.cast());
+  } finally {
+    calloc..free(what)..free(pResult);
+  }
+}
