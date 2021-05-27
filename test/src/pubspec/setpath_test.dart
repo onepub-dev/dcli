b091be42734ca5dc5dab8072de5ed29a559d48a8
Brett Sutton <bsutton@noojee.com.au>
2021-05-27 17:52:54 +1000

    additional registry tests.

--- test/src/pubspec/setpath_test.dart
+++ test/src/pubspec/setpath_test.dart
@@ -5,6 +5,16 @@ import 'package:dcli/src/util/pub_cache.dart';
 import 'package:dcli/windows.dart';
 import 'package:path/path.dart';
 import 'package:test/test.dart';
+import 'package:win32/win32.dart';
+
+void _appendIfAbsent(String newPath) {
+  final path =
+      regGetExpandString(HKEY_CURRENT_USER, 'Environment', 'Path');
+
+  if (!path.contains(newPath)) {
+    regAppendToPath(newPath);
+  }
+}
 
 void main() {
   test('setPath', () {
@@ -16,8 +26,10 @@ void main() {
     Env().addToPATHIfAbsent(Settings().pathToDCliBin);
 
     print(PATH);
-
-    replacePath(PATH);
+    // update the windows registry so the change sticks.
+    _appendIfAbsent(join(dartToolDir, 'bin'));
+    _appendIfAbsent(PubCache().pathToBin);
+    _appendIfAbsent(Settings().pathToDCliBin);
 
     // 'setx PATH "${PATH.join(Env().delimiterForPATH)}"'.run;
   }, skip: !Platform.isWindows);
