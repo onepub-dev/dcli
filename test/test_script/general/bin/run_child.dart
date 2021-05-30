2ea59a7952a5451eb0fbc3db9ed82a3d089c15b4
Brett Sutton <bsutton@noojee.com.au>
2021-05-30 22:05:39 +1000

    Change to calling dart scripts from run_child.dart to explicty use dart as windows won't run the script unless we do it via a shell which then doesn't pass the called scripts exit code.

--- test/test_script/general/bin/run_child.dart
+++ test/test_script/general/bin/run_child.dart
@@ -1,10 +1,22 @@
 #! /usr/bin/env dcli
 
+import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 
 ///
 /// This script will '.run' a child script passed as
 ///  the first and only argument.
 void main(List<String> args) {
-  args[0].run;
+  final script = args[0];
+
+  if (Platform.isWindows) {
+    if (script.endsWith('.dart')) {
+      'dart $script'.run;
+    } else {
+      script.run;
+    }
+  } else {
+    script.run;
+  }
 }
