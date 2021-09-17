0a161ac48750840c30aa0870178a79ae0d224155
Brett Sutton <bsutton@noojee.com.au>
2021-09-17 12:20:48 +1000

    Changed example to use the new Platform().eol.

--- example/example.dart
+++ example/example.dart
@@ -1,5 +1,7 @@
 #! /usr/bin/env dcli
 
+import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 
 void main() {
@@ -87,7 +89,7 @@ void main() {
     // Review our good woork.
     cat(restingPlace);
 
-    read(restingPlace, delim: Platform.eol).forEach(print);
+    read(restingPlace, delim: Platform().eol).forEach(print);
 
     // ask the user if we are ready to publish.
     // But we can't do this in a vscode debug session
