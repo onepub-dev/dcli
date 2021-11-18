07efabe29f53394aaf0319ed662909c249e8ba78
Brett Sutton <bsutton@noojee.com.au>
2021-11-18 23:07:31 +1100

    ran pack.

---
+++ lib/src/dcli/resources/generated/Edeaaadfbebef.g.dart
@@ -0,0 +1,20 @@
+// ignore: prefer_relative_imports
+import 'package:dcli/dcli.dart';
+
+/// GENERATED -- GENERATED
+/// 
+/// DO NOT MODIFIY
+/// 
+/// This script is generated via [Resource.pack()].
+/// 
+/// GENERATED - GENERATED
+
+class Edeaaadfbebef extends PackedResource {
+
+  /// PackedResource - test.me
+  const Edeaaadfbebef() : super(
+     '''
+SGVsbG93IHdvcmxkLgo=
+  ''');
+}
+    
--- lib/src/dcli/resources/generated/resource_registry.g.dart
+++ lib/src/dcli/resources/generated/resource_registry.g.dart
@@ -1,27 +1,28 @@
 // ignore: prefer_relative_imports
 import 'package:dcli/dcli.dart';
-import 'Ddbab.g.dart';
-import 'Fcddbb.g.dart';
+import 'Bcecccfedcafdbf.g.dart';
+import 'Edeaaadfbebef.g.dart';
 
 /// GENERATED -- GENERATED
-///
+/// 
 /// DO NOT MODIFIY
-///
+/// 
 /// This script is generated via [Resource.pack()].
-///
+/// 
 /// GENERATED - GENERATED
 
 class ResourceRegistry {
+
   /// Map of the packed files.
   /// Use the path of a packed file (relative to the resource directory)
   /// to access the packed resource and then call [PackedResource].unpack()
   /// to unpack the file.
   /// ```dart
-  /// ResourceRegistry.resources['rules.yaml'].unpack(join(HOME, '.mysettings'
-  /// , 'rules.yaml'));
+  /// ResourceRegistry.resources['rules.yaml'].unpack(join(HOME, '.mysettings', 'rules.yaml'));
   /// ```
   static const Map<String, PackedResource> resources = {
-    'PXL_20211104_224740653.jpg': Ddbab(),
-    'test.me': Fcddbb(),
-  };
-}
+      'PXL_20211104_224740653.jpg' : Bcecccfedcafdbf(),
+            'test.me' : Edeaaadfbebef(),
+          };
+  }
+  
