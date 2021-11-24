bfe7e2b15b6d3ec63258b65dde0c505d025a4a2e
Brett Sutton <bsutton@noojee.com.au>
2021-11-24 19:33:21 +1100

    test for wmic

---
+++ issues/wmic.dart
@@ -0,0 +1,23 @@
+import 'package:dcli/dcli.dart';
+import 'package:dcli/src/util/string_as_process.dart';
+
+void main(List<String> args) {
+  Settings().setVerbose(enabled: true);
+  try {
+    'wmic process get processid,parentprocessid,executablepath'.run;
+
+    final processes =
+        'wmic process get processid,parentprocessid,executablepath'
+            .toList(skipLines: 1);
+    print(processes.join('\n'));
+  } on FormatException catch (e, st) {
+    print('message: ${e.message}');
+    print('offset: ${e.offset}');
+    print('source: ${e.source}');
+    print(st);
+    // ignore: avoid_catches_without_on_clauses
+  } catch (e, st) {
+    print(e.toString());
+    print(st);
+  }
+}
