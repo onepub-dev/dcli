49a3f13cfe7f05424b3806292ff78927a1abec5a
Brett Sutton <bsutton@noojee.com.au>
2021-11-25 14:07:17 +1100

    Added windows specific shells to windows.dart barrel file.

--- lib/windows.dart
+++ lib/windows.dart
@@ -1,5 +1,9 @@
-/// barrel file for windows specific code.
+/// barrel file for windows specific functions.
+library windows;
+
 export 'package:win32/win32.dart';
 
 export 'src/platform/windows/registry.dart';
+export 'src/shell/cmd_shell.dart';
+export 'src/shell/power_shell.dart';
 export 'src/shell/windows_mixin.dart';
