2ff6042de5d095032ff406085150401764b3f8d6
Brett Sutton <bsutton@noojee.com.au>
2021-12-05 09:11:23 +1100

    modified activate_local so you can run it from within the project.

--- tool/activate_local.dart
+++ tool/activate_local.dart
@@ -1,13 +1,19 @@
 #! /usr/bin/env dcli
 
+import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 
 /// globally activates dcli from a local path rather than a public package.
 ///
 ///
 void main(List<String> args) {
+  if (!Shell.current.isPrivilegedUser) {
+    printerr(red(Shell.current.privilegesRequiredMessage('activate_local')));
+    exit(1);
+  }
   final root = DartProject.self.pathToProjectRoot;
-  PubCache().globalActivateFromSource(dirname(root));
+  PubCache().globalActivateFromSource(root);
   'dcli install'.start();
-  PubCache().globalActivateFromSource(dirname(root));
+  PubCache().globalActivateFromSource(root);
 }
