0f35f0dc9d87ca80d964ef8937904c22d84ba112
Brett Sutton <bsutton@noojee.com.au>
2021-12-10 14:20:34 +1100

    regenerated registry

--- dcli/lib/src/dcli/resource/generated/resource_registry.g.dart
+++ dcli/lib/src/dcli/resource/generated/resource_registry.g.dart
@@ -1,15 +1,33 @@
 // ignore: prefer_relative_imports
 import 'package:dcli/dcli.dart';
+import 'Acafafaacadbc.g.dart';
+import 'Adbbdfddb.g.dart';
+import 'Aeadabce.g.dart';
+import 'Afbcafebadceccbc.g.dart';
+import 'Afbcefabc.g.dart';
+import 'Cbebeaacaabaced.g.dart';
+import 'Dccaaadfcef.g.dart';
+import 'Deecebaadae.g.dart';
+import 'Dfcbacaf.g.dart';
+import 'Ebdacffddaef.g.dart';
+import 'Edddcfdacbfeebc.g.dart';
+import 'Eeabddeeee.g.dart';
+import 'Eeefaafdfaa.g.dart';
+import 'Eefebcdadfcaacd.g.dart';
+import 'Fceceddecb.g.dart';
+import 'Fceeafcceafa.g.dart';
+import 'Ffffffbaefcc.g.dart';
 
 /// GENERATED -- GENERATED
-///
+/// 
 /// DO NOT MODIFIY
-///
+/// 
 /// This script is generated via [Resource.pack()].
-///
+/// 
 /// GENERATED - GENERATED
 
 class ResourceRegistry {
+
   /// Map of the packed files.
   /// Use the path of a packed file (relative to the resource directory)
   /// to access the packed resource and then call [PackedResource].unpack()
@@ -18,5 +36,24 @@ class ResourceRegistry {
   /// ResourceRegistry.resources['rules.yaml']
   ///     .unpack(join(HOME, '.mysettings', 'rules.yaml'));
   /// ```
-  static const resources = <String, PackedResource>{};
-}
+  static const resources = <String, PackedResource>{
+      'template/hello_world/bin/main.dart' : Acafafaacadbc(),
+            'template/cmd_args/pubspec.yaml' : Adbbdfddb(),
+            'template/basic/pubspec.lock' : Aeadabce(),
+            'template/basic/analysis_options.yaml' : Afbcafebadceccbc(),
+            'template/find/analysis_options.yaml' : Afbcefabc(),
+            'template/cmd_args/bin/main.dart' : Cbebeaacaabaced(),
+            'template/find/bin/main.dart' : Dccaaadfcef(),
+            'template/hello_world/pubspec.yaml' : Deecebaadae(),
+            'template/basic/bin/main.dart' : Dfcbacaf(),
+            'template/hello_world/analysis_options.yaml' : Ebdacffddaef(),
+            'template/hello_world/pubspec.lock' : Edddcfdacbfeebc(),
+            'template/cmd_args/analysis_options.yaml' : Eeabddeeee(),
+            'template/cmd_args/pubspec.lock' : Eeefaafdfaa(),
+            'template/find/bin/find.dart' : Eefebcdadfcaacd(),
+            'template/find/pubspec.lock' : Fceceddecb(),
+            'template/basic/pubspec.yaml' : Fceeafcceafa(),
+            'template/find/pubspec.yaml' : Ffffffbaefcc(),
+          };
+  }
+  
