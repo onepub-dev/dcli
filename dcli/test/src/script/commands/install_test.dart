b6666ea79ef26759dcd62c07318e8316fd14c631
Brett Sutton <bsutton@noojee.com.au>
2021-12-29 10:00:55 +1100

    Fixed the install test to work with the new template structure.

--- dcli/test/src/script/commands/install_test.dart
+++ dcli/test/src/script/commands/install_test.dart
@@ -3,12 +3,9 @@ import 'dart:io';
 
 import 'package:dcli/dcli.dart' hide equals;
 import 'package:dcli/src/script/commands/install.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../mocks/mock_env.dart';
-import '../../mocks/mock_settings.dart';
-import '../../util/test_file_system.dart';
+import '../../util/test_scope.dart';
 
 void main() {
   group(
@@ -17,15 +14,18 @@ void main() {
       test(
         'warmup install',
         () {
-          expect(!Platform.isWindows || Shell.current.isPrivilegedUser, isTrue);
-          TestFileSystem(useCommonPath: false).withinZone((fs) {
+          withTestScope((testDir) {
+            expect(
+                !Platform.isWindows || Shell.current.isPrivilegedUser, isTrue);
+            //TestFileSystem(useCommonPath: false).withinZone((fs) {
+
             try {
-              Shell.current.install();
+              InstallCommand().run([], []);
             } on DCliException catch (e) {
               print(e);
             }
 
-            checkInstallStructure(fs);
+            checkInstallStructure(testDir);
 
             // Now install over existing
             try {
@@ -34,55 +34,34 @@ void main() {
               print(e);
             }
 
-            checkInstallStructure(fs);
+            checkInstallStructure(testDir);
           });
         },
         tags: ['privileged'],
       );
-      test('add ~/.dcli/bin to PATH on Windows', () {
-        TestFileSystem().withinZone((fs) {
-          final settings = Settings();
-          final mockSettings = MockSettings();
-          final mockEnv = MockEnv();
-
-          // windows we can't add a path just expect user message.
-          when(() => mockSettings.isWindows).thenReturn(true);
-          when(() => mockSettings.isLinux).thenReturn(false);
-          when(() => mockSettings.isMacOS).thenReturn(false);
-          when(() => mockSettings.isVerbose).thenReturn(false);
-          when(() => mockSettings.pathToDCliBin)
-              .thenReturn(settings.pathToDCliBin);
-
-          when(() => mockEnv.HOME).thenReturn(r'C:\windows\userdata');
-          when(() => mockEnv.isOnPATH(settings.pathToDCliBin))
-              .thenReturn(false);
-
-          Settings.mock = mockSettings;
-          Env.mock = mockEnv;
-        });
-      });
 
       test('set env PATH Linux', () {
-        TestFileSystem().withinZone((fs) {
-          final settings = Settings();
-          final mockSettings = MockSettings();
-          final mockEnv = MockEnv();
+        // TestFileSystem().withinZone((fs) {
+        //   final settings = Settings();
+        //   final mockSettings = MockSettings();
+        //   final mockEnv = MockEnv();
 
-          when(() => mockSettings.isWindows).thenReturn(false);
-          when(() => mockSettings.isLinux).thenReturn(true);
-          when(() => mockSettings.isMacOS).thenReturn(false);
-          when(() => mockSettings.isVerbose).thenReturn(false);
-          when(() => mockSettings.pathToDCliBin)
-              .thenReturn(settings.pathToDCliBin);
+        //   when(() => mockSettings.isWindows).thenReturn(false);
+        //   when(() => mockSettings.isLinux).thenReturn(true);
+        //   when(() => mockSettings.isMacOS).thenReturn(false);
+        //   when(() => mockSettings.isVerbose).thenReturn(false);
+        //   when(() => mockSettings.pathToDCliBin)
+        //       .thenReturn(settings.pathToDCliBin);
 
-          when(() => mockEnv.HOME).thenReturn(HOME);
-          when(() => mockEnv.isOnPATH(settings.pathToDCliBin))
-              .thenReturn(false);
+        //   when(() => mockEnv.HOME).thenReturn(HOME);
+        //   when(() => mockEnv.isOnPATH(settings.pathToDCliBin))
+        //       .thenReturn(false);
 
-          Settings.mock = mockSettings;
-          Env.mock = mockEnv;
+        //   Settings.mock = mockSettings;
+        //   Env.mock = mockEnv;
 
-          final export = 'export PATH=\$PATH:${settings.pathToDCliBin}';
+        withTestScope((testDir) {
+          final export = 'export PATH=\$PATH:${Settings().pathToDCliBin}';
 
           final profilePath = join(HOME, '.profile');
           if (exists(profilePath)) {
@@ -90,9 +69,8 @@ void main() {
               ..retainWhere((line) => line.startsWith('export'));
             expect(exportLines, contains(export));
           }
-          Env.reset();
-          Settings.reset();
         });
+        // });
       });
 
       test('With Lib', () {});
@@ -105,27 +83,55 @@ void main() {
   });
 }
 
-void checkInstallStructure(TestFileSystem fs) {
-  expect(exists(truepath(HOME, '.dcli')), equals(true));
+void checkInstallStructure(String home) {
+  expect(exists(truepath(home, '.dcli')), equals(true));
+  expect(
+      exists(truepath(home, '.dcli', 'template', 'project', 'custom')), isTrue);
+  expect(exists(truepath(home, '.dcli', 'template', 'project')), isTrue);
 
-  expect(exists(truepath(HOME, '.dcli', 'template')), equals(true));
+  checkProjectStructure(home, 'simple');
+  checkProjectStructure(home, 'cmd_args');
+  checkProjectStructure(home, 'find');
+  checkProjectStructure(home, 'full');
+  checkProjectStructure(home, 'simple');
+
+  checkScriptStructure(home);
+}
+
+void checkScriptStructure(String home) {
+  expect(exists(truepath(home, '.dcli', 'template', 'script')), equals(true));
+  expect(exists(truepath(home, '.dcli', 'template', 'script', 'custom')),
+      equals(true));
+  expect(exists(truepath(home, '.dcli', 'template', 'script', 'simple.dart')),
+      equals(true));
+  expect(
+      exists(truepath(
+          home, '.dcli', 'template', 'script', 'analysis_options.yaml')),
+      equals(true));
+  expect(exists(truepath(home, '.dcli', 'template', 'script', 'pubspec.lock')),
+      equals(true));
+  expect(exists(truepath(home, '.dcli', 'template', 'script', 'pubspec.yaml')),
+      equals(true));
+}
+
+void checkProjectStructure(String home, String projectName) {
+  final projectPath =
+      truepath(home, '.dcli', 'template', 'project', projectName);
+  expect(exists(projectPath), equals(true));
 
   final templates =
-      find('*.*', workingDirectory: join(fs.home!, '.dcli', 'template'))
-          .toList();
-
-  final base = join(fs.home!, '.dcli', 'template');
+      find('*', includeHidden: true, workingDirectory: projectPath).toList();
 
   expect(
     templates,
     unorderedEquals(
       <String>[
-        absolute(base, 'basic.dart'),
-        absolute(base, 'hello_world.dart'),
-        absolute(base, 'pubspec.yaml.template'),
-        absolute(base, 'README.md'),
-        absolute(base, 'analysis_options.yaml.template'),
-        absolute(base, 'cmd_args.dart'),
+        truepath(projectPath, 'bin', 'main.dart'),
+        truepath(projectPath, 'analysis_options.yaml'),
+        truepath(projectPath, 'pubspec.lock'),
+        truepath(projectPath, 'pubspec.yaml'),
+        truepath(projectPath, 'README.md'),
+        truepath(projectPath, 'CHANGELOG.md'),
       ],
     ),
   );
