d0c61495fd10cd46e8a6952c538053b38b5cc613
Brett Sutton <bsutton@noojee.com.au>
2022-03-05 13:55:05 +1100

    moved chmod and chown to posix package.

---
+++ dcli/lib/src/posix/chmod.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:dcli_core/dcli_core.dart' as core;
+import 'package:dcli_core/dcli_core.dart' hide exists;
+import 'package:posix/posix.dart' as posix;
+
+import '../../dcli.dart';
+
+/// Wrapper for the linux `chmod` command.
+///
+/// [permission] is an octal string as used by the cli commandchmod e.g. 777
+/// [path] is the path to the file that we are changing the
+/// permissions of.
+///
+/// The [permission] digits are intrepeted as owner, group, other.
+/// So:
+/// 641
+/// owner - 6
+/// group - 4
+/// other - 1
+///
+/// Each digit is the sum of the permissions:
+/// 4 - allow read
+/// 2 - allow write
+/// 1 - all execute
+///
+/// So 6 is 4 + 2 is read and write and from the above example gives the owner r/w permission.
+///
+/// To set give the owner execution privileges use:
+/// ```dart
+/// chmod('/path/to/exe', '100');
+/// ```
+/// If [path] doesn't exist a ChModException] is thrown.
+///
+/// On Windows a call to this method is a noop.
+///
+void chmod(String path, {required String permission}) =>
+    _ChMod()._chmod(path, permission);
+
+/// Implementatio for [chmod] function.
+class _ChMod extends DCliFunction {
+// this.user, this.group, this.other, this.path
+
+  void _chmod(String path, String permission) {
+    if (!exists(path)) {
+      throw ChModException('The file at ${truepath(path)} does not exists');
+    }
+    if (!Platform.isWindows) {
+      if (posix.isPosixSupported) {
+        posix.chmod(path, permission);
+      } else {
+        'chmod $permission "$path"'.run;
+      }
+    }
+  }
+
+  //  String chmod({int user, int group, int other, this.path}) {}
+
+/*  String buildPermission(int permission) {
+    bool read = ((permission & 4) >> 2) == 1;
+    bool write = ((permission & 2) >> 1) == 1;
+    bool execute = ((permission & 1)) == 1;
+  }
+  */
+}
+
+/// Thrown if the [chmod] function encounters an error.
+class ChModException extends DCliFunctionException {
+  /// Thrown if the [chmod] function encounters an error.
+  ChModException(String reason, [core.StackTraceImpl? stacktrace])
+      : super(reason, stacktrace);
+
+  // @override
+  // DCliException copyWith(core.StackTraceImpl stackTrace) =>
+  //     ChModException(message, stackTrace);
+}
---
+++ dcli/lib/src/posix/chown.dart
@@ -0,0 +1,73 @@
+import 'package:dcli_core/dcli_core.dart' as core;
+import 'package:posix/posix.dart' as _posix;
+
+import '../../dcli.dart';
+
+/// Sets the owner of a file on posix systems.
+///
+/// Changes the user or group ownership of [path].
+///
+/// On Windows this command has no effect.
+///
+/// [path] is the path to the file or directory that we are changing the
+/// ownership of. If [path] does not exists then a [ChOwnException] is thrown.
+/// [path] may be absolute (preferred) or relative.
+///
+/// [user] is the posix user that will own the file/directory. If no [user] is specified
+/// then the loggedin user is used.
+///
+/// [group] is the posix group that will own the file/directory. If no [group] is specified
+/// then [user] is used as the group name.
+///
+/// If [recursive] is true (the default) then the change is applied to
+///  all subdirectories.
+/// If you pass [recursive] and [path] is a file then [recursive]
+/// will be ignored.
+///
+void chown(String path, {String? user, String? group, bool recursive = true}) =>
+    _ChOwn()._chown(path, user: user, group: group, recursive: recursive);
+
+/// Implementatio for [chown] function.
+class _ChOwn extends core.DCliFunction {
+// this.user, this.group, this.other, this.path
+
+  void _chown(
+    String path, {
+    String? user,
+    String? group,
+    bool recursive = true,
+  }) {
+    if (Settings().isWindows) {
+      return;
+    }
+
+    user ??= Shell.current.loggedInUser ?? '';
+
+    group ??= user;
+    if (!exists(path)) {
+      throw ChOwnException(
+        'The file/directory at ${truepath(path)} does not exists',
+      );
+    }
+
+    final passwd = _posix.getpwnam(user);
+    final pgroup = _posix.getgrnam(group);
+    if (isDirectory(path) && recursive) {
+      find('*', includeHidden: true, workingDirectory: path)
+          .forEach((file) => _posix.chown(path, passwd.uid, pgroup.gid));
+    } else {
+      _posix.chown(path, passwd.uid, pgroup.gid);
+    }
+  }
+}
+
+/// Thrown if the [chown] function encounters an error.
+class ChOwnException extends core.DCliFunctionException {
+  /// Thrown if the [chown] function encounters an error.
+  ChOwnException(String reason, [core.StackTraceImpl? stacktrace])
+      : super(reason, stacktrace);
+
+  // @override
+  // DCliException copyWith(core.StackTraceImpl stackTrace) =>
+  //     ChOwnException(message, stackTrace);
+}
