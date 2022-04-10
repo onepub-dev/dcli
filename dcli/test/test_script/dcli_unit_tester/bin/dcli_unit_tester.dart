57f8fbfe6f4f38dbb86772aad392004dea5f0ab4
Brett Sutton <bsutton@noojee.com.au>
2022-04-11 07:44:37 +1000

    corrected the dcli_core import version due to bug in pub_release. updated dlci_unit_tester to use latest version of dcli.

--- dcli/test/test_script/dcli_unit_tester/bin/dcli_unit_tester.dart
+++ dcli/test/test_script/dcli_unit_tester/bin/dcli_unit_tester.dart
@@ -36,56 +36,57 @@ void main(List<String> args) {
   } else if (parsed['script'] as bool) {
     dumpScript();
   } else {
-    print(DartScript.current.pathToScript);
+    print(DartScript.self.pathToScript);
   }
 }
 
 void dumpScript() {
   final widths = <int>[30, -1];
-  dump('basename', DartScript.current.basename, widths: widths);
-  dump('exeName', DartScript.current.exeName, widths: widths);
-  dump('isCompiled', DartScript.current.isCompiled, widths: widths);
-  dump('isInstalled', DartScript.current.isInstalled, widths: widths);
-  dump('isPubGlobalActivated', DartScript.current.isPubGlobalActivated,
+  dump('basename', DartScript.self.basename, widths: widths);
+  dump('exeName', DartScript.self.exeName, widths: widths);
+  dump('isCompiled', DartScript.self.isCompiled, widths: widths);
+  dump('isInstalled', DartScript.self.isInstalled, widths: widths);
+  dump('isPubGlobalActivated', DartScript.self.isPubGlobalActivated,
       widths: widths);
-  dump('isReadyToRun', DartScript.current.isReadyToRun, widths: widths);
-  dump('pathToExe', DartScript.current.pathToExe, widths: widths);
-  dump('pathToInstalledExe', DartScript.current.pathToInstalledExe,
+  dump('isReadyToRun', DartScript.self.isReadyToRun, widths: widths);
+  dump('pathToExe', DartScript.self.pathToExe, widths: widths);
+  dump('pathToInstalledExe', DartScript.self.pathToInstalledExe,
       widths: widths);
-  dump('pathToProjectRoot', DartScript.current.pathToProjectRoot,
+  dump('pathToProjectRoot', DartScript.self.pathToProjectRoot, widths: widths);
+  dump('pathToPubSpec', DartScript.self.pathToPubSpec, widths: widths);
+  dump('pathToScript', DartScript.self.pathToScript, widths: widths);
+  dump('pathToScriptDirectory', DartScript.self.pathToScriptDirectory,
       widths: widths);
-  dump('pathToPubSpec', DartScript.current.pathToPubSpec, widths: widths);
-  dump('pathToScript', DartScript.current.pathToScript, widths: widths);
-  dump('pathToScriptDirectory', DartScript.current.pathToScriptDirectory,
-      widths: widths);
-  dump('scriptName', DartScript.current.scriptName, widths: widths);
+  dump('scriptName', DartScript.self.scriptName, widths: widths);
 }
 
 void dump(String label, Object value, {required List<int> widths}) {
-  print(Format.row([label, value.toString()], widths: widths));
+  print(Format().row([label, value.toString()], widths: widths));
 }
 
 void dumpPlatform() {
   final widths = <int>[30, -1];
-  print(Format.row(['executable:', Platform.executable], widths: widths));
-  print(Format.row(['executableArguments:', '${Platform.executableArguments}'],
+  print(Format().row(['executable:', Platform.executable], widths: widths));
+  print(Format().row(
+      ['executableArguments:', '${Platform.executableArguments}'],
       widths: widths));
-  print(Format.row(['isLinux: ', '${Platform.isLinux}'], widths: widths));
-  print(Format.row(['isWindows: ', '${Platform.isWindows}'], widths: widths));
-  print(Format.row(['isMacOS:', '${Platform.isMacOS}'], widths: widths));
-  print(Format.row(['localeName: ', (Platform.localeName)], widths: widths));
-  print(
-      Format.row(['localHostname:', (Platform.localHostname)], widths: widths));
-  print(Format.row(['numberOfProcessors: ', '${Platform.numberOfProcessors}'],
+  print(Format().row(['isLinux: ', '${Platform.isLinux}'], widths: widths));
+  print(Format().row(['isWindows: ', '${Platform.isWindows}'], widths: widths));
+  print(Format().row(['isMacOS:', '${Platform.isMacOS}'], widths: widths));
+  print(Format().row(['localeName: ', (Platform.localeName)], widths: widths));
+  print(Format()
+      .row(['localHostname:', (Platform.localHostname)], widths: widths));
+  print(Format().row(['numberOfProcessors: ', '${Platform.numberOfProcessors}'],
       widths: widths));
-  print(Format.row(['operatingSystem: ', (Platform.operatingSystem)],
-      widths: widths));
-  print(Format.row(
+  print(Format()
+      .row(['operatingSystem: ', (Platform.operatingSystem)], widths: widths));
+  print(Format().row(
       ['operatingSystemVersion:', (Platform.operatingSystemVersion)],
       widths: widths));
-  print(Format.row(['packageConfig:', Platform.packageConfig], widths: widths));
   print(
-      Format.row(['pathSeparator:', (Platform.pathSeparator)], widths: widths));
-  print(Format.row(['script:', '${Platform.script}'], widths: widths));
-  print(Format.row(['version:', (Platform.version)], widths: widths));
+      Format().row(['packageConfig:', Platform.packageConfig], widths: widths));
+  print(Format()
+      .row(['pathSeparator:', (Platform.pathSeparator)], widths: widths));
+  print(Format().row(['script:', '${Platform.script}'], widths: widths));
+  print(Format().row(['version:', (Platform.version)], widths: widths));
 }
