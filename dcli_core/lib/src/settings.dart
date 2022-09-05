99857646415d090b6384120b5510eaa75529d0ba
Brett Sutton <bsutton@noojee.com.au>
2022-09-05 17:18:07 +1000

    back ported fixes to setVerbose which was producing duplicated logging output.

--- dcli_core/lib/src/settings.dart
+++ dcli_core/lib/src/settings.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:logging/logging.dart';
 
 import 'util/dcli_platform.dart';
@@ -20,6 +21,9 @@ class Settings {
   /// dcli -v clean
   bool get isVerbose => _verbose;
 
+  // ignore: cancel_subscriptions
+  static StreamSubscription<LogRecord>? listener;
+
   /// Turns on verbose logging.
   void setVerbose({required bool enabled}) {
     _verbose = enabled;
@@ -31,13 +35,15 @@ class Settings {
 
     if (enabled) {
       logger.level = Level.INFO;
-      logger.onRecord.listen((record) {
+      listener ??= logger.onRecord.listen((record) {
         print('${record.level.name}: ${record.time}: ${record.message}');
       });
     } else {
-      logger
-        ..level = Level.OFF
-        ..clearListeners();
+      logger.level = Level.OFF;
+      if (listener != null) {
+        listener!.cancel();
+        listener = null;
+      }
     }
   }
 
