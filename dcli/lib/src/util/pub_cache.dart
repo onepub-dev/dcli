c0bcb084914d1d99d27d8d38f2573c326b74a273
Brett Sutton <bsutton@noojee.com.au>
2022-11-15 17:28:48 +1100

    Fix: back ported fix for PubCache.findPrimaryVersion which failed if the hosted directory didn't exist.

--- dcli/lib/src/util/pub_cache.dart
+++ dcli/lib/src/util/pub_cache.dart
@@ -176,6 +176,9 @@ class PubCache {
   ///
   /// If no versions are installed then null is returned.
   Version? findPrimaryVersion(String packageName) {
+    if (!exists(pathToDartLang)) {
+      return null;
+    }
     final packages = find(
       '$packageName-*.*',
       types: [Find.directory],
