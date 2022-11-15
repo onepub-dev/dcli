75fef62343550cc7feede0e352a29801b822d267
Brett Sutton <bsutton@noojee.com.au>
2022-11-15 18:40:23 +1100

    repacked files.

---
+++ dcli/lib/src/dcli/resource/generated/Ad68874eb9d33258c597d856a8b4539db.g.dart
@@ -0,0 +1,38 @@
+// ignore: prefer_relative_imports
+import 'package:dcli/dcli.dart';
+
+/// GENERATED -- GENERATED
+///
+/// DO NOT MODIFIY
+///
+/// This script is generated via [Resource.pack()].
+///
+/// GENERATED - GENERATED
+
+class Ad68874eb9d33258c597d856a8b4539db extends PackedResource {
+  /// PackedResource - ../../template/project/full/analysis_options.yaml
+  const Ad68874eb9d33258c597d856a8b4539db();
+
+  /// A hash of the resource (pre packed) calculated by
+  /// [calculateHash].
+  /// This hash can be used to check if the resource needs to
+  /// be updated on the target system.
+  /// Use :
+  /// ```dart
+  ///   calculateHash(pathToResource).hexEncode() == packResource.checksum
+  /// ```
+  /// to compare the checksum of the local file with
+  /// this checksum
+  @override
+  String get checksum =>
+      'a397a958643f8943e2fac4eea1916f1f163d9874e5b2ee24952703c798c8ed44';
+
+  /// <package>/resources relative path to the original resource.
+  @override
+  String get originalPath => 'template/project/full/analysis_options.yaml';
+
+  @override
+  String get content => '''
+aW5jbHVkZTogcGFja2FnZTpsaW50X2hhcmQvYWxsLnlhbWwK
+  ''';
+}
