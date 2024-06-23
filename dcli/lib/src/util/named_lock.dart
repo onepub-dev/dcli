7d20be59d571f50eac516a9642f93e12ae7b407d
Brett Sutton <bsutton@noojee.com.au>
2024-06-23 21:46:52 +1000

    temporary cleanup of the withLock methods until we get a real fix for the runtime lock package.

--- dcli/lib/src/util/named_lock.dart
+++ dcli/lib/src/util/named_lock.dart
@@ -4,9 +4,8 @@
  * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
  */
 
-import 'dart:async' show FutureOr;
-
 import 'package:runtime_named_locks/runtime_named_locks.dart' as runtime;
+import 'package:runtime_named_locks/runtime_named_locks.dart';
 import 'package:stack_trace/stack_trace.dart' show Trace;
 
 import '../../dcli.dart' show DCliException, verbose;
@@ -61,22 +60,24 @@ class NamedLock {
   /// a log message to the console.
   ///
   /// Throws a [DCliException] if the NamedLock times out.
-  FutureOr<void> withLock(
-    FutureOr<void> Function() action, {
+  Future<T> withLock<T>(
+    T Function() action, {
     String? waiting,
   }) async {
     final callingStackTrace = Trace.current();
 
-    final execution = runtime.ExecutionCall<FutureOr<void>, DCliException>(
+    final execution = runtime.ExecutionCall<T, DCliException>(
       callable: () => action(),
       safe: true,
     );
+
+    ExecutionCall<T, DCliException> _execution;
 
     try {
       verbose(() => 'withLock called for $_nameWithSuffix');
 
       // Note that guarded here is the same insance as execution above
-      final _execution = runtime.NamedLock.guard<FutureOr<void>, DCliException>(
+      _execution = runtime.NamedLock.guard<T, DCliException>(
         name: _nameWithSuffix,
         execution: execution,
         waiting: waiting,
@@ -85,7 +86,6 @@ class NamedLock {
 
       if (_execution.successful.isSet && _execution.successful.get!) {
         await _execution.completer.future;
-        await _execution.returned;
       } else if (_execution.error.isSet) {
         await _execution.error.get?.rethrow_();
       }
@@ -98,6 +98,53 @@ class NamedLock {
     } finally {
       verbose(() => 'withLock completed for $_nameWithSuffix');
     }
+    return _execution.returned;
+  }
+
+  /// creates a lock file and then calls [action]
+  /// once [action] returns the lock is released.
+  /// If [waiting] is passed it will be used to write
+  /// a log message to the console.
+  ///
+  /// Throws a [DCliException] if the NamedLock times out.
+  Future<T> withLockAsync<T>(
+    Future<T> Function() action, {
+    String? waiting,
+  }) async {
+    final callingStackTrace = Trace.current();
+
+    final execution = runtime.ExecutionCall<Future<T>, DCliException>(
+      callable: () => action(),
+      safe: true,
+    );
+
+    ExecutionCall<Future<T>, DCliException> _execution;
+    try {
+      verbose(() => 'withLock called for $_nameWithSuffix');
+
+      // Note that guarded here is the same insance as execution above
+      _execution = runtime.NamedLock.guard<Future<T>, DCliException>(
+        name: _nameWithSuffix,
+        execution: execution,
+        waiting: waiting,
+        timeout: _timeout,
+      );
+
+      if (_execution.successful.isSet && _execution.successful.get!) {
+        await await _execution.completer.future;
+      } else if (_execution.error.isSet) {
+        await _execution.error.get?.rethrow_();
+      }
+    } on DCliException catch (e) {
+      verbose(() => 'Exception caught for $_nameWithSuffix...  $e : $e');
+      throw e..stackTrace = callingStackTrace;
+    } on Exception catch (e) {
+      verbose(() => 'Exception caught for $_nameWithSuffix... $e : $e');
+      throw DCliException.from(e, callingStackTrace);
+    } finally {
+      verbose(() => 'withLock completed for $_nameWithSuffix');
+    }
+    return _execution.returned;
   }
 }
 
