3beff7988a245fa3cc0b289781162ef555130a6b
Brett Sutton <bsutton@noojee.com.au>
2024-07-22 18:31:47 +1000

    reverted windows mix deprecated as the version ranges stil requires use of the deprecated versions.

--- dcli/lib/src/shell/windows_mixin.dart
+++ dcli/lib/src/shell/windows_mixin.dart
@@ -139,7 +139,7 @@ mixin WindowsMixin {
             (pElevation) {
           if (OpenProcessToken(
                 GetCurrentProcess(),
-                TOKEN_QUERY,
+                TOKEN_ACCESS_MASK.TOKEN_QUERY,
                 phToken.cast(),
               ) ==
               1) {
