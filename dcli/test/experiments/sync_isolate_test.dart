2a9fcf1de94d11bd5c8d5812e5e0a1005358edfa
Brett Sutton <bsutton@noojee.com.au>
2024-07-31 11:48:02 +1000

    additional tests for sync isolates.

--- dcli/test/experiments/sync_isolate_test.dart
+++ dcli/test/experiments/sync_isolate_test.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'dart:io' as io;
+import 'dart:io';
 import 'dart:isolate';
 import 'dart:typed_data';
 
@@ -11,8 +12,11 @@ import 'package:dcli/src/process/process/message.dart';
 import 'package:dcli/src/process/process/message_response.dart';
 import 'package:dcli/src/process/process/process_in_isolate.dart';
 import 'package:dcli/src/process/process/process_settings.dart';
+import 'package:dcli/src/util/isolate_id.dart';
+import 'package:dcli_test/dcli_test.dart';
 import 'package:native_synchronization/mailbox.dart';
 import 'package:native_synchronization/sendable.dart';
+import 'package:path/path.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -26,15 +30,33 @@ void main() {
   test('unit_tester', () {
     'dcli_unit_tester --ask'.start(terminal: true);
   });
+
+  test('test file system', () async {
+    await TestFileSystem().withinZone((fs) async {
+      await DartProject.self.warmup();
+    });
+  });
+
+  test('spawn', () async {
+    const lockName = 'spawn_test';
+    await withTempDirAsync((lockPath) async {
+      final isolate = isolateID;
+      touch(join(lockPath, '.$pid.$isolate.$lockName'), create: true);
+
+      // await NamedLock(name: lockName).withLockAsync(() async {
+      _spawn(1);
+      _spawn(2);
+      _spawn(3);
+    });
+  });
+  // });
 }
 
-void firstline() {
-  'getent passwd bsutton'.firstLine!.split(':');
-}
-
-void test4() {
+void _spawn(int index) {
+  // await NamedLock(name: lockName).withLockAsync(() async {
   final isolateChannel = IsolateChannel(
-    process: ProcessSettings('which', args: ['which']),
+    process: ProcessSettings('dcli_unit_tester',
+        args: ['-n', '-l', '5', '-o', '$index']),
   );
 
   startIsolate(isolateChannel);
@@ -50,6 +72,10 @@ void test4() {
       });
   } while (response.messageType != MessageType.exitCode);
   print(orange('primary: received exit message'));
+}
+
+void firstline() {
+  'getent passwd bsutton'.firstLine!.split(':');
 }
 
 void _run(String arg) {
